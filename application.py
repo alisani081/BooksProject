0268a35774a152e64f49aff62af8ac43a0ea0815
Ali Sani <alisani081@yahoo.com>
2019-10-18 14:26:56 +0100

    working registration program

--- application.py
+++ application.py
@@ -1,12 +1,14 @@
 from dotenv import load_dotenv
 import os
 
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, request, redirect, url_for, flash
+from werkzeug.security import check_password_hash, generate_password_hash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
 
 #Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -26,8 +28,36 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
     return render_template("index.html")
 
-@app.route("/signup")
+@app.route("/signup", methods=["POST","GET"])
 def signup():
+    if request.method == "POST":
+        name = request.form.get('name')
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        error = None
+
+        if not name:
+            error = "All fields are required!"
+
+        if db.execute("SELECT username FROM users WHERE username = :username", {"username": username}
+        ).fetchone() is not None:
+            error = f"{username} already exist!"            
+
+        if error is None:
+            password = generate_password_hash(password)
+            user = db.execute("INSERT INTO users (name, email, username, password) VALUES (:name, :email, :username, :password) RETURNING id",
+                {"name": name, "email": email, "username": username, "password": password}) 
+            
+            db.commit()
+            
+            flash('Account created successfully! Use your details to login now.', 'success')
+
+            return redirect(url_for('login'))
+        
+        flash(error, 'danger')
+
     return render_template("signup.html")
 
 @app.route("/login")
--- import.py
+++ import.py
@@ -16,12 +16,13 @@ def main():
     reader = csv.reader(f)
     next(reader, None)
 
-    # insert rows into db
-    for isbn, title, name, year in reader:
-        db.execute("INSERT INTO authors (name) VALUES (:name)", {"name": name})
-    print("Inserted!")
+    # Insert rows into db
+    for isbn, title, author, year in reader:
+        db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)", 
+            {"isbn": isbn, "title": title, "author": author, "year": year})
+    print("Books Inserted")
     db.commit()
-    
+
 
 if __name__ == "__main__":
     main()
